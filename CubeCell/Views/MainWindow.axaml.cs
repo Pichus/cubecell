4ae0cb5ee83e9c3d79072faada47801925744cd3
pichus <dimatyzuk@gmail.com>
2025-10-18 14:13:51 +0300

    extend the spreadsheet component provided by the library so it triggers a custom event when any cell is changed

--- CubeCell/Views/MainWindow.axaml.cs
+++ CubeCell/Views/MainWindow.axaml.cs
@@ -1,4 +1,9 @@
+using System;
+using System.Diagnostics;
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Avalonia.Interactivity;
 
 namespace CubeCell.Views;
 
@@ -7,5 +12,12 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        
+        MySpreadSheet.CellValueChanged += OnCellValueChanged;
+    }
+    
+    private void OnCellValueChanged(object? sender, CellValueChangedEventArgs e)
+    {
+        Console.WriteLine($"Cell [{e.Column}, {e.Row}] changed from '{e.OldValue}' to '{e.NewValue}'");
     }
 }
