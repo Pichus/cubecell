250f7a99bd411d2c622ad27023a9953d847c4288
pichus <dimatyzuk@gmail.com>
2025-10-19 15:53:50 +0300

    create a proper welcome page

--- src/CubeCell.App/ViewModels/WelcomePageViewModel.cs
+++ src/CubeCell.App/ViewModels/WelcomePageViewModel.cs
@@ -1,10 +1,16 @@
+using System.Collections.ObjectModel;
 using System.Reactive;
+using System.Windows.Input;
 using ReactiveUI;
 
 namespace CubeCell.App.ViewModels;
 
 public class WelcomePageViewModel : ViewModelBase, IRoutableViewModel
 {
+    private int _colCount = 26;
+
+    private int _rowCount = 100;
+
     public WelcomePageViewModel(IScreen screen)
     {
         HostScreen = screen;
@@ -12,14 +18,37 @@ public class WelcomePageViewModel : ViewModelBase, IRoutableViewModel
         GoToAboutPageCommand =
             ReactiveCommand.CreateFromObservable(() =>
                 HostScreen.Router.Navigate.Execute(new AboutPageViewModel(HostScreen)));
-        
+
         GoToSpreadSheetEditorPageCommand =
             ReactiveCommand.CreateFromObservable(() =>
-                HostScreen.Router.Navigate.Execute(new SpreadsheetEditorPageViewModel(HostScreen)));
+                HostScreen.Router.Navigate.Execute(
+                    new SpreadsheetEditorPageViewModel(HostScreen, _rowCount, _colCount)));
+    }
+
+    public int RowCount
+    {
+        get => _rowCount;
+        set => this.RaiseAndSetIfChanged(ref _rowCount, value);
+    }
+
+    public int ColCount
+    {
+        get => _colCount;
+        set => this.RaiseAndSetIfChanged(ref _colCount, value);
     }
 
     public ReactiveCommand<Unit, IRoutableViewModel> GoToAboutPageCommand { get; }
     public ReactiveCommand<Unit, IRoutableViewModel> GoToSpreadSheetEditorPageCommand { get; }
+
+    public bool IsGoogleAuthorized { get; set; }
+    public string GoogleUserName { get; set; }
+    public string GoogleUserEmail { get; set; }
+    public string GoogleUserInitial { get; set; }
+    public ObservableCollection<string> RecentGoogleFiles { get; set; }
+    public ICommand GoogleSignInCommand { get; }
+    public ICommand GoogleSignOutCommand { get; }
+    public ICommand OpenGoogleFileCommand { get; }
+    public ICommand RefreshGoogleFilesCommand { get; }
 
     public string? UrlPathSegment { get; } = "welcome";
     public IScreen HostScreen { get; }
