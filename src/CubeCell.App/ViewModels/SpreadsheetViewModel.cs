42d88fb1683c637263b9a401cab856de4f3a8ff0
pichus <dimatyzuk@gmail.com>
2025-10-26 20:46:46 +0200

    create a spreadsheet view model

---
+++ src/CubeCell.App/ViewModels/SpreadsheetViewModel.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+
+using CubeCell.App.Models;
+using CubeCell.App.Utils;
+
+namespace CubeCell.App.ViewModels;
+
+public class SpreadsheetViewModel
+{
+    private readonly Dictionary<CellCoordinates, CellViewModel> _cellViewModels = new();
+
+    public void SetCell(CellCoordinates coordinates, CellViewModel cell)
+    {
+        _cellViewModels[coordinates] = cell;
+    }
+
+    public CellViewModel? GetCell(int col, int row)
+    {
+        _cellViewModels.TryGetValue(new CellCoordinates(col, row), out CellViewModel? cell);
+
+        return cell ?? null;
+    }
+
+    public CellViewModel? GetCell(string address)
+    {
+        var (col, row) = CellAddressUtils.AddressToCoordinates(address);
+        return GetCell(col, row) ?? null;
+    }
+}
